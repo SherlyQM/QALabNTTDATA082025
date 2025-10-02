be0464ee1cd1c3df31bfca812df68de6b99d3de9
SherlyQM <squispemaucaylle753@gmail.com>
2025-10-01 22:03:05 -0500

    "Subiendo desde el intellij clase Hyundai"

---
+++ POO_Game/src/Hyundai.java
@@ -0,0 +1,10 @@
+
+// Clase creada por Sherly Quispe Maucaylle
+public class Hyundai extends Auto{
+
+    Hyundai(String sPlaca){
+        //Inicializando
+        super(sPlaca);
+        super.setdVelocidadMaxima(200);
+    }
+}
